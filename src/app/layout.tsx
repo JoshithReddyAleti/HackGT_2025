0dfb433748ebd6e508fa1447e8479697d023c9b1
Isabelle Ilyia <isabelle@cedar.law>
2025-09-20 15:50:39 -0700

    message renderer for mastra tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { CedarCopilot, ProviderConfig } from 'cedar-os';
+import { messageRenderers } from '@/cedar/messageRenderers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <CedarCopilot llmProvider={llmProvider}>{children}</CedarCopilot>
+        <CedarCopilot llmProvider={llmProvider} messageRenderers={messageRenderers}>
+          {children}
+        </CedarCopilot>
       </body>
     </html>
   );
